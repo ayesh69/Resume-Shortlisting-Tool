a9120180b878d6d17d3723f1289756235f5e5dd8
Ayesh <ayeshvitharana@gmail.com>
2025-08-11 12:58:09 +1000

    test

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -55,7 +55,7 @@ const getProfile = async (req, res) => {
 const updateUserProfile = async (req, res) => {
     try {
         const user = await User.findById(req.user.id);
-        if (!user) return res.status(404).json({ message: 'User not found' });
+        if (!user) return res.status(404).json({ message: 'User not found.' });
 
         const { name, email, university, address } = req.body;
         user.name = name || user.name;
