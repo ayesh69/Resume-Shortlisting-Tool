42ff9a746ef11d96c8b7a6c2ad8a682214b09c00
Ayesh <ayeshvitharana@gmail.com>
2025-08-11 14:44:22 +1000

    cicd test comment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -38,7 +38,7 @@ const getProfile = async (req, res) => {
     try {
       const user = await User.findById(req.user.id);
       if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'User not found.' });
       }
   
       res.status(200).json({
