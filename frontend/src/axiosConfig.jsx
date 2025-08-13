465f1db4f4f18d6ab1d6c576d4227950439e4297
Ayesh <ayeshvitharana@gmail.com>
2025-08-13 19:31:42 +1000

    RST-14 < Create the resume upload component with drag-and-drop interface.>

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://54.206.102.181:5001', // live
+  baseURL: 'http://13.210.224.24:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
