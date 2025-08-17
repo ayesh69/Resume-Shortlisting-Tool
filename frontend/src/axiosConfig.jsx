bec2782cf7803c332a34635f5253f846c59d7251
Ayesh <ayeshvitharana@gmail.com>
2025-08-17 20:47:06 +1000

    error fixing

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,21 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://13.210.221.110:5001', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://13.210.221.110:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
+
+axiosInstance.interceptors.request.use(
+  config => {
+    // Grab token from localStorage (or wherever you store it)
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
 export default axiosInstance;
